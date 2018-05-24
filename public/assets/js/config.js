b1fe4d5161783bb23edef18b54978a2bb678354c
Alex <werwolfx2003@mail.ru>
2018-05-24 22:01:33 +0300

    init

---
+++ public/assets/js/config.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+  apiKey: "авра лвар ывларыв лар ывл",
+  authDomain: "test.firebaseapp.com",
+  databaseURL: "https://test.firebaseio.com",
+  storageBucket: "test.com",
+  messagingSenderId: "45234523423"
+};
+firebase.initializeApp(config);
